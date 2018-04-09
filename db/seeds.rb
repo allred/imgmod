d420211a718beccc6b1b5de93c08c9f8a9980d8e
Mike Allred <mikejallred@gmail.com>
2018-04-09 08:50:17 -0400

    Added controller, seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+path_input = "#{Rails.root}/images.csv"
+File.open(path_input, 'r') do |f|
+  f.each_line do |l|
+    time_entered, url = l.chomp.split(/\s*,\s*/)
+    Image.create({
+      time_entered: DateTime.strptime(time_entered, '%s'),
+      url: url,
+    })
+  end
+end
+=begin
+images = Image.create([
+  {time_entered: 1455123632, url: "https://d3ous0vnp05zqm.cloudfront.net/manual_uploads/moderation_challenge/images/4.1.01.jpeg"},
+  {time_entered: 1455123633, url: "https://d3ous0vnp05zqm.cloudfront.net/manual_uploads/moderation_challenge/images/4.1.02.jpeg"},
+])
+=end
