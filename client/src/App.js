4d65e9f81f508ec08ee4f7a7fef3d38ffc1f599a
Mike Allred <mikejallred@gmail.com>
2018-04-09 14:19:18 -0400

    Minimal dashboard.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import Dashboard from './resources/Dashboard'
 import './App.css';
+
+const Moderate = () => (
+  <div>
+    <h2>Moderate</h2>
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+          <div>
+            <ul>
+              <li><Link to="/moderate">Moderate</Link></li>
+              <li><Link to="/dashboard">Dashboard</Link></li>
+            </ul>
+            <hr/>
+           <Route path="/moderate" component={Moderate}/>
+           <Route path="/dashboard" component={Dashboard}/>
+          </div>
+        </Router>
       </div>
     );
   }
